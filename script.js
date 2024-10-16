c8a39aaf3f106c6708ba9a5cb447117f620c631d
Bhavchandani <bhavchandani@gmail.com>
2024-10-16 20:30:08 +1100

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
 console.log("run")
 
 function handleSubmit(event) {
-    event.preventDefault(); // Prevent the default form submission
+    event.preventDefault();
     
-    // Show alert message
+    
     alert('Thanks for your message. We will be in touch.');
 
-    // Reset the form
+    
     document.getElementById('contactForm').reset();
 }
 
-/*modal window js*/ 
 function openModal(modalId) {
     document.getElementById(modalId).style.display = "block";
 }
@@ -19,7 +18,6 @@ function closeModal(modalId) {
     document.getElementById(modalId).style.display = "none";
 }
 
-// Close modal when clicking outside of the modal content
 window.onclick = function(event) {
     const modals = document.getElementsByClassName('modal');
     for (let i = 0; i < modals.length; i++) {
